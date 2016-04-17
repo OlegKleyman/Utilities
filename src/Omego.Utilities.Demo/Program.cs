c13559d6522a74def311059e15420c390a1a0881
Oleg Kleyman <Omego2K@gmail.com>
2016-04-17 16:52:42 -0400

    add demo project

---
+++ src/Omego.Utilities.Demo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Omego.Utilities.Demo
+{
+    public static class Program
+    {
+        public static void Main(params string[] args)
+        {
+
+        }
+    }
+}
